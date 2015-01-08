0d26d827a46c073b084480d9c5e9010b857f6701
Angelo Scotto <a.scotto@reply.it>
2015-01-08 16:00:21 +0100

    Added REDIS launch & termination.

--- tests/MultiServerLockTests.cs
+++ tests/MultiServerLockTests.cs
@@ -8,6 +8,7 @@ using MbUnit.Framework;
 
 using Redlock.CSharp;
 using StackExchange.Redis;
+using System.Diagnostics;
 
 namespace Redlock.CSharp.Tests
 {
@@ -15,6 +16,51 @@ namespace Redlock.CSharp.Tests
     public class MultiServerLockTests
     {
         private const string resourceName = "MyResourceName";
+        private List<Process> redisProcessList = new List<Process>();
+       
+        [FixtureSetUp]
+        public void setup()
+        {
+            // Launch Server
+            Process redis = new Process();
+
+            // Configure the process using the StartInfo properties.
+            redis.StartInfo.FileName = System.IO.Path.GetFullPath(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"..\..\..\packages\Redis-32.2.6.12.1\tools\redis-server.exe");
+            redis.StartInfo.Arguments = "--port 6379";
+            redis.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            redis.Start();
+            redisProcessList.Add(redis);
+
+            redis = new Process();
+
+            // Configure the process using the StartInfo properties.
+            redis.StartInfo.FileName = System.IO.Path.GetFullPath(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"..\..\..\packages\Redis-32.2.6.12.1\tools\redis-server.exe");
+            redis.StartInfo.Arguments = "--port 6380";
+            redis.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            redis.Start();
+            redisProcessList.Add(redis);
+
+            redis = new Process();
+
+            // Configure the process using the StartInfo properties.
+            redis.StartInfo.FileName = System.IO.Path.GetFullPath(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"..\..\..\packages\Redis-32.2.6.12.1\tools\redis-server.exe");
+            redis.StartInfo.Arguments = "--port 6381";
+            redis.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            redis.Start();
+
+            redisProcessList.Add(redis);
+        }
+
+        [FixtureTearDown]
+        public void teardown()
+        {
+            foreach (var process in redisProcessList)
+            {
+                if (!process.HasExited) process.Kill();
+            }
+
+            redisProcessList.Clear();
+        }
 
         [Test]
         public void TestWhenLockedAnotherLockRequestIsRejected()
