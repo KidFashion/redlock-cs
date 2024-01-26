fd518041d09baa83a9c9cb498ae59597ea65b37e
nimanikoo <nikoonazar.nima@gmail.com>
2024-01-26 18:26:18 +0330

    Fix Indent and Rm Unused Using also change tha variables which can  Inline variable declaration in SingleServerLockTests.cs and MultiServerLockTests.cs and app.json

--- src/redislock-cs.test/MultiServerLockTests.cs
+++ src/redislock-cs.test/MultiServerLockTests.cs
@@ -1,25 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using NUnit.Framework;
 using Microsoft.Extensions.Configuration;
-
-using Redlock.CSharp;
 using StackExchange.Redis;
-using System.Diagnostics;
 
 namespace Redlock.CSharp.Tests
 {
     [TestFixture]
     public class MultiServerLockTests
     {
-        private const string resourceName = "MyResourceName";
-        private const string ServerA_Key = "ConnectionString_ServerA";
-        private const string ServerB_Key = "ConnectionString_ServerB";
-        private const string ServerC_Key = "ConnectionString_ServerC";
+        private const string ResourceName = "MyResourceName";
+        private const string ServerAKey = "ConnectionString_ServerA";
+        private const string ServerBKey = "ConnectionString_ServerB";
+        private const string ServerCKey = "ConnectionString_ServerC";
 
 #if TOREMOVE
         //contains list of processes for teardown
@@ -34,7 +26,8 @@ namespace Redlock.CSharp.Tests
             Process redis = new Process();
 
             // Configure the process using the StartInfo properties.
-            redis.StartInfo.FileName = System.IO.Path.GetFullPath(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"..\..\..\packages\Redis-32.2.6.12.1\tools\redis-server.exe");
+            redis.StartInfo.FileName =
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             System.IO.Path.GetFullPath(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"..\..\..\packages\Redis-32.2.6.12.1\tools\redis-server.exe");
             redis.StartInfo.Arguments = "--port 6379";
             redis.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             redis.Start();
@@ -43,7 +36,8 @@ namespace Redlock.CSharp.Tests
             redis = new Process();
 
             // Configure the process using the StartInfo properties.
-            redis.StartInfo.FileName = System.IO.Path.GetFullPath(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"..\..\..\packages\Redis-32.2.6.12.1\tools\redis-server.exe");
+            redis.StartInfo.FileName =
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       System.IO.Path.GetFullPath(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"..\..\..\packages\Redis-32.2.6.12.1\tools\redis-server.exe");
             redis.StartInfo.Arguments = "--port 6380";
             redis.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             redis.Start();
@@ -52,7 +46,8 @@ namespace Redlock.CSharp.Tests
             redis = new Process();
 
             // Configure the process using the StartInfo properties.
-            redis.StartInfo.FileName = System.IO.Path.GetFullPath(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"..\..\..\packages\Redis-32.2.6.12.1\tools\redis-server.exe");
+            redis.StartInfo.FileName =
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 System.IO.Path.GetFullPath(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"..\..\..\packages\Redis-32.2.6.12.1\tools\redis-server.exe");
             redis.StartInfo.Arguments = "--port 6381";
             redis.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             redis.Start();
@@ -79,41 +74,37 @@ namespace Redlock.CSharp.Tests
             configBuilder
                 .AddJsonFile("app.json", true);
             var configRoot = configBuilder.Build();
-            
-            var dlm = new Redlock(ConnectionMultiplexer.Connect(configRoot[ServerA_Key]), ConnectionMultiplexer.Connect(configRoot[ServerB_Key]), ConnectionMultiplexer.Connect(configRoot[ServerC_Key]));
 
-            Lock lockObject;
-            Lock newLockObject;
+            var dlm = new Redlock(ConnectionMultiplexer.Connect(configRoot[ServerAKey]),
+                ConnectionMultiplexer.Connect(configRoot[ServerBKey]),
+                ConnectionMultiplexer.Connect(configRoot[ServerCKey]));
 
-            var locked = dlm.Lock(resourceName, new TimeSpan(0, 0, 10), out lockObject);
+            var locked = dlm.Lock(ResourceName, new TimeSpan(0, 0, 10), out var lockObject);
             Assert.IsTrue(locked, "Unable to get lock");
-            locked = dlm.Lock(resourceName, new TimeSpan(0, 0, 10), out newLockObject);
+            locked = dlm.Lock(ResourceName, new TimeSpan(0, 0, 10), out _);
             Assert.IsFalse(locked, "lock taken, it shouldn't be possible");
             dlm.Unlock(lockObject);
         }
 
         [Test]
-        public void TestThatSequenceLockedUnlockedAndLockedAgainIsSuccessfull()
+        public void TestThatSequenceLockedUnlockedAndLockedAgainIsSuccessfully()
         {
             var configBuilder = new ConfigurationBuilder();
             configBuilder
                 .AddJsonFile("app.json", true);
             var configRoot = configBuilder.Build();
 
-            var dlm = new Redlock(ConnectionMultiplexer.Connect(configRoot[ServerA_Key]), ConnectionMultiplexer.Connect(configRoot[ServerB_Key]), ConnectionMultiplexer.Connect(configRoot[ServerC_Key]));
+            var dlm = new Redlock(ConnectionMultiplexer.Connect(configRoot[ServerAKey]),
+                ConnectionMultiplexer.Connect(configRoot[ServerBKey]),
+                ConnectionMultiplexer.Connect(configRoot[ServerCKey]));
 
-            Lock lockObject = null;
-            Lock newLockObject;
-
-            var locked = dlm.Lock(resourceName, new TimeSpan(0, 0, 10), out lockObject);
+            var locked = dlm.Lock(ResourceName, new TimeSpan(0, 0, 10), out var lockObject);
             Assert.IsTrue(locked, "Unable to get lock");
             dlm.Unlock(lockObject);
-            locked = dlm.Lock(resourceName, new TimeSpan(0, 0, 10), out newLockObject);
+            locked = dlm.Lock(ResourceName, new TimeSpan(0, 0, 10), out var newLockObject);
             Assert.IsTrue(locked, "Unable to get lock");
 
             dlm.Unlock(newLockObject);
-
-
         }
     }
 }
